0772634672860bc96f029a477f473853c0de3922
Anastasia C <clarkanastasia@outlook.com>
2024-02-26 12:32:22 +0000

    add methods for Bank class

--- BankManagement/Bank.cs
+++ BankManagement/Bank.cs
@@ -4,7 +4,25 @@ public class Bank
 {
     public string BankName {get; init;} = string.Empty;
 
-    public readonly HashSet<Account> Accounts = [];
+    public readonly HashSet<Account> _accounts = [];
 
     public readonly HashSet<Transaction> Transactions = [];
+
+    public void AddAccount(string name)
+    {
+        var newAccount = new Account 
+        {
+            AccountHolder = name,
+        };
+        _accounts.Add(newAccount);
+    }
+    
+    public void GetAccounts()
+    {
+        Console.WriteLine($"Here is a list of all the account holders at: ${BankName}:");
+        foreach (Account account in _accounts)
+        {
+            Console.WriteLine(account.AccountHolder);
+        }
+    }
 }
