3bb5f6e53ec8bdb0944519123e4544298f82a5d4
Anastasia C <clarkanastasia@outlook.com>
2024-02-26 14:56:33 +0000

    add methods to filter transactions

--- BankManagement/Bank.cs
+++ BankManagement/Bank.cs
@@ -20,14 +20,15 @@ public class Bank
     
     public void GetAccounts()
     {
-        Console.WriteLine($"Here is a list of all the account holders at: ${BankName}:");
+        Console.WriteLine($"Here is a list of all the account holders at: {BankName}:");
         foreach (Account account in _accounts)
         {
             Console.WriteLine(account.AccountHolder);
         }
     }
 
-    public Account FindAccount(string name){
+    public Account FindAccount(string name)
+    {
         var accountToFind = new Account
         {
             AccountHolder = name,
@@ -52,11 +53,33 @@ public class Bank
 
     public void GetTransactions()
     {
-        Console.WriteLine($"Here is a list of all the transactions at: ${BankName}:");
+        Console.WriteLine($"Here is a list of all the transactions at {BankName}:");
         foreach (Transaction t in _transactions)
         {
             Console.WriteLine($"{t.From.AccountHolder} gave {t.To.AccountHolder} {t.Amount} for {t.Narrative} on {t.Date}");
         }
     }
-    
+
+    public void GetTransactionsByName(string name)
+    {
+        IEnumerable<Transaction> query = _transactions.Where((t) => t.From.AccountHolder == name);
+        Console.WriteLine($"Here are all the transactions involving {name}: ");
+        foreach (Transaction t in query)
+        {
+            Console.WriteLine($"{t.Amount} to {t.To.AccountHolder} for {t.Narrative} on {t.Date}");
+        }   
+    }
+    public void GetAllTransactionsBySum()
+    {
+        foreach (Account account in _accounts)
+        {
+        IEnumerable<Transaction> query = _transactions.Where((t) => t.To.Equals(account));
+            decimal totalAmount = 0;
+            foreach (Transaction t in query)
+            {
+                totalAmount = query.Sum(t => t.Amount);
+            }
+        Console.WriteLine($"{account.AccountHolder} owes a total of {totalAmount}");
+        }
+    }
 }
