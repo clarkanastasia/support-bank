5576da26aebe5f0c708e4b60e23dbdc962c6d3d6
Anastasia C <clarkanastasia@outlook.com>
2024-02-26 14:57:17 +0000

    add Bank property

--- BankManagement/Account.cs
+++ BankManagement/Account.cs
@@ -2,13 +2,15 @@ namespace SupportBank.BankManagement;
 
 public class Account
 {
-    public string AccountHolder {get; set;} = string.Empty;
+    public required string AccountHolder {get; set;} = string.Empty;
+    
+    public required Bank Bank {get; init;}
     
     public override bool Equals(object? obj)
     {
         return obj is Account account && account.AccountHolder == AccountHolder;
     }
-    
+
     public override int GetHashCode()
     {
         return AccountHolder.GetHashCode();
