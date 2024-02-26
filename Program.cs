68af932bccd15d506472c64a478e42abd086ae3a
Anastasia C <clarkanastasia@outlook.com>
2024-02-26 11:47:09 +0000

    create file reader for csv

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using SupportBank.BankManagement;
+
+var myTransactions = new List<string>();
+
+string fileName = "";
+try
+{
+    var reader = new StreamReader(fileName);
+    // Console.WriteLine("Here is the content of your file:");
+    // Console.WriteLine(reader.ReadToEnd().Trim());
+    string line; 
+
+    while ((line = reader.ReadLine().Trim()) != null)
+    {
+        myTransactions.Add(line);
+    }
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("Sorry, that file was not found.");
+}
