c16b0e547739ce79cbe59fcc6eee507da07170f4
Anastasia C <clarkanastasia@outlook.com>
2024-02-26 12:33:21 +0000

    add override methods for Account class

--- BankManagement/Account.cs
+++ BankManagement/Account.cs
@@ -2,5 +2,15 @@ namespace SupportBank.BankManagement;
 
 public class Account
 {
-    public string AccountHolder {get; set;}
+    public string AccountHolder {get; set;} = string.Empty;
+    
+    public override bool Equals(object? obj)
+    {
+        return obj is Account account && account.AccountHolder == AccountHolder;
+    }
+    
+    public override int GetHashCode()
+    {
+        return AccountHolder.GetHashCode();
+    }
 }
