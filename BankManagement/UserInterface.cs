76d8b5db1e0c5dffe0e3e5febeb8c788db444b5b
Anastasia C <clarkanastasia@outlook.com>
2024-02-27 12:35:05 +0000

    add logger statements

--- BankManagement/UserInterface.cs
+++ BankManagement/UserInterface.cs
@@ -1,10 +1,14 @@
 namespace SupportBank.BankManagement;
+using NLog;
 
 public class UserInterface
 {
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
     public Bank Bank {get; init;}
     public void Run()
     {
+    Logger.Info("App has started running");
     ReadFile();
     DisplayMenu();
     }
@@ -12,6 +16,7 @@ public class UserInterface
     public void DisplayMenu()
     {
     Console.WriteLine($"Welcome to {Bank.BankName}!");
+    Logger.Info("User has started using the menu");
     bool isFinished;
     do
     {
@@ -31,7 +36,6 @@ public class UserInterface
                     Bank.GetTransactionsByName(name);
                     break;
                 case "4":
-                    isFinished = true;
                     break;
                 default:
                     Console.WriteLine("The option you selected is not valid");       
@@ -40,7 +44,8 @@ public class UserInterface
             Console.WriteLine("Do you want to see the menu again? \n 1. Yes \n 2. No");
             input = Console.ReadLine() ?? "";
             isFinished = input == "2";
-        }while(!isFinished);
+        } while(!isFinished);
+    Logger.Info("User has finished using the menu");    
     } 
 
     public void ReadFile()
@@ -55,7 +60,10 @@ public class UserInterface
     {
         // Console.WriteLine("Please enter the file you would like to read");
         // var fileName = Console.ReadLine() ?? "";
-        string fileName = "Transactions2014.csv";
+        // string fileName = "Transactions2014.csv";
+        string fileName = "DodgyTransactions2015.csv";
+        Logger.Info($"The selected file is {fileName}");
+
         return fileName;
     } 
 
@@ -76,10 +84,12 @@ public class UserInterface
                     Bank.AddAccount(parts[1]);
                     Bank.AddAccount(parts[2]);
             }
+        Logger.Info("Finished getting account names from file");    
+        reader.Close();    
         }
-        catch (FileNotFoundException)
+        catch (Exception ex)
         {
-            Console.WriteLine("Sorry, that file was not found.");
+            Logger.Warn($"Failed to create Account instance: {ex.Message}");
         }
     }
 
@@ -92,23 +102,35 @@ public class UserInterface
             string line; 
             while ((line = reader.ReadLine()) != null)
             {
-                if (isFirstLine){
+                if (isFirstLine)
+                {
                     isFirstLine = false;
                     continue;
                 }
                     string[] parts = line.Split(",");
-                    var date = parts[0];
+                    DateTime date = default;
+                    if (DateTime.TryParse(parts[0], out DateTime dateValue))
+                    {
+                    date = dateValue;
+                    }
                     var from = parts[1];
                     var to = parts[2];
                     var narrative = parts[3];
-                    var amount = Convert.ToDecimal(parts[4]);
+                    decimal amount = 0;
+                    try {
+                    amount = Convert.ToDecimal(parts[4]);
+                        } 
+                    catch (Exception ex ){
+                    Logger.Warn($"Failed to convert to decimal: {ex.Message}");
+                    }
                     Bank.AddTransaction(amount, narrative, from, to, date);
-                    
             }
+        Logger.Info("Finished getting transaction info from file");    
+        reader.Close();    
         }
-        catch (FileNotFoundException)
+        catch (Exception ex)
         {
-            Console.WriteLine("Sorry, that file was not found.");
+            Logger.Error($"Failed to create Transaction instance: {ex.Message}");
         }
     }
 }
