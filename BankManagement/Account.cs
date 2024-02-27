8623a662db31766ef4fc8542407b55beb61c5910
Anastasia C <clarkanastasia@outlook.com>
2024-02-27 15:24:27 +0000

    remove default empty string from AccountHolder property

--- BankManagement/Account.cs
+++ BankManagement/Account.cs
@@ -2,11 +2,11 @@ namespace SupportBank.BankManagement;
 
 public class Account
 {
-    public required string AccountHolder {get; set;} = string.Empty;
+    public required string AccountHolder {get; set;}
     
     public required Bank Bank {get; init;}
     
-    public override bool Equals(object obj)
+    public override bool Equals(object? obj)
     {
         return obj is Account account && account.AccountHolder == AccountHolder;
     }
